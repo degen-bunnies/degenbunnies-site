b58c41a3ab56c8f9552c6e812af5ed667051ec93
combineads <combine@combineads.co.kr>
2023-09-01 00:22:10 +0900

    Add priority property of Image tag

--- src/app/(WebSite)/components/pages/about-us/Profile.tsx
+++ src/app/(WebSite)/components/pages/about-us/Profile.tsx
@@ -17,8 +17,8 @@ export default function Profile({name, role, children}: ProfileProp) {
     <ReactCompareSlider
         position={90}
         onlyHandleDraggable={true}
-        itemOne={<Image src={`/images/avatar/${name}-left.png`} alt={`${name} avatar`} width={500} height={500} style={{width: '100%', height: 'auto'}}/>}
-        itemTwo={<Image src={`/images/avatar/${name}-right.png`} alt={`${name} avatar`} width={500} height={500} style={{width: '100%', height: 'auto'}}/>}/>
+        itemOne={<Image src={`/images/avatar/${name}-left.png`} alt={`${name} avatar`} width={500} height={500} priority style={{width: '100%', height: 'auto'}}/>}
+        itemTwo={<Image src={`/images/avatar/${name}-right.png`} alt={`${name} avatar`} width={500} height={500} priority style={{width: '100%', height: 'auto'}}/>}/>
     <Typography className={bigShoulders.className} sx={{mt: '0.5rem', textAlign: 'center', textTransform: 'uppercase', fontWeight: '600', fontSize: '3rem'}}>
       {name}
     </Typography>
